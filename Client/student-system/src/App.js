4fe33ee1eafeb79f2eec45391187afbae6138c2f
Akshat0102 <76106653+Akshat0102@users.noreply.github.com>
2022-04-22 22:25:31 +0530

    added navbar with name-logo

--- Client/student-system/src/App.js
+++ Client/student-system/src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/home/home.component';
+import NavBar from './components/navbar/navbar.component';
 
 function App() {
   return (
     <div className="App">
-      Hello World
+      <NavBar/>
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
     </div>
   );
 }
--- Client/student-system/src/components/navbar/navbar.component.jsx
+++ Client/student-system/src/components/navbar/navbar.component.jsx
@@ -1,19 +1,23 @@
-// import React from "react";
-// import './navbar.styles.css';
+import React from "react";
+import './navbar.styles.css';
+import { Link } from 'react-router-dom';
+import logo from '../../assets/images/StudentInfoSys.svg'
 
-// const NavBar = () => {
-//     return (
-//         <div className="navbar">
-//             <nav className="nav-wrap">
-//                 <Link to={'/'} className='link' ><p>HOME</p></Link>
-//                 <Link to={'/about'} className='link'><p>ABOUT</p></Link>
-//                 <Link to={'/skills'} className='link'><p>SKILLS</p></Link>
-//                 <Link to={'/designs'} className='link' ><p>DESIGNS</p></Link>
-//                 <Link to={'/projects'} className='link'><p>PROJECTS</p></Link>
-//                 <Link to={'/resume'} className='link'><p>RESUME</p></Link>
-//             </nav>
-//         </div>
-//     )
-// }
+const NavBar = () => {
+    return (
+        <div className="navbar">
+            <div className="name-logo">
+                <img src={logo} alt="StudentInfoSys" />
+            </div>
+            <nav className="nav-wrap">
+                <Link to={'/team'} className='link' ><p>Team</p></Link>
+                <Link to={'/about'} className='link'><p>About us</p></Link>
+                <Link to={'/contact'} className='link'><p>Contact</p></Link>
+                <Link to={'/sign-up'} className='link' ><p>Sign Up</p></Link>
+                <Link to={'/log-in'} className='link'><p>Log in</p></Link>
+            </nav>
+        </div>
+    )
+}
 
-// export default NavBar;
+export default NavBar;

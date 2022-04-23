dab0e57a7549f68ca0e1ac2dd8e1ae8f9c2ec3f8
Dhruv Dave <78278281+DhruvDave12@users.noreply.github.com>
2022-04-23 17:51:16 +0530

    added config

--- Server/app.js
+++ Server/app.js
@@ -3,14 +3,12 @@ const express  = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-const studentRoutes = require('./routes/student.routes');
 const authRoutes = require('./routes/auth.routes');
 const connection = require('./config/db');
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}))
 
-app.use('/', studentRoutes);
 app.use('/', authRoutes);
 
 app.get('/', (req,res) => {
---
+++ Server/config/db.js
@@ -0,0 +1,11 @@
+const mysql = require('mysql2');
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    database: process.env.DATABASE_NAME,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD
+})
+
+module.exports = connection;

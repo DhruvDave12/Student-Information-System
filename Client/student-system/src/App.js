0451cc0887693e10c9851f883b4d00104ed307b6
Akshat0102 <76106653+Akshat0102@users.noreply.github.com>
2022-04-27 19:32:34 +0530

    created team page and profile of teammates

--- Client/student-system/src/App.js
+++ Client/student-system/src/App.js
@@ -8,11 +8,13 @@ import StudentDataForm from './pages/student-dataform/student-data-form.componen
 import FacultyDataForm from './pages/faculty-dataform/faculty-data-form.component';
 import AcademicsDataForm from './pages/academics-dataform/academics-data-form.component';
 import InternshipDataForm from './pages/internship-dataform/internship-data-form.component';
-import StudentData from './pages/student-data/student-data.component';
-import StudentProfile from './pages/student-profile/student_profile.component';
-import FacultyProfile from './pages/faculty-data/faculty_profile.component';
 import CurricularDataForm from './pages/curricular-dataform/curricular-data-form.component';
 import CourseDataForm from './pages/courses-dataform/courses-data-form.component';
+import StudentData from './pages/student-data/student-data.component';
+import FacultyProfile from './pages/faculty-data/faculty_profile.component';
+import StudentProfile from './pages/student-profile/student_profile.component';
+import TeamData from './components/teamdata/teamdata.component';
+import Team from './pages/team/team.component';
 
 function App() {
   return (
@@ -22,12 +24,14 @@ function App() {
         <Route path='/' element={<Home />} />
         <Route path='/login' element={<Login />} />
         <Route path='/signup' element={<SignUp />} />
+        <Route path='/team' element={<Team />} />
         <Route path='/student-dataform' element={<StudentDataForm />} />
         <Route path='/faculty-dataform' element={<FacultyDataForm />} />
         <Route path='/academics-dataform' element={<AcademicsDataForm />} />
         <Route path='/internship-dataform' element={<InternshipDataForm />} />
         <Route path='/curricular-dataform' element={<CurricularDataForm />} />
         <Route path='/course-dataform' element={<CourseDataForm />} />
+        <Route path='/teamdata' element={<TeamData />} />
         <Route path='/info' element={<StudentData />} />
         <Route path='/student/:id' element={<StudentProfile />} />
         <Route path='/faculty/:id' element={<FacultyProfile />} />

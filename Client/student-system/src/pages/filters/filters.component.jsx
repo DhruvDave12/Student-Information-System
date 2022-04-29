921f1f7ef2f87d38c2e4400dc3db68ce602d3b5a
Dhruv Dave <78278281+DhruvDave12@users.noreply.github.com>
2022-04-29 14:37:53 +0530

    added filter new structure

--- Client/student-system/src/pages/filters/filters.component.jsx
+++ Client/student-system/src/pages/filters/filters.component.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./filters.styles.css";
 import FilterFaculty from "../../components/filter_components/faculty_filter/filter_faculty.component";
 import CurricularFilter from "../../components/filter_components/curricular/curricular_filter.component";
@@ -6,16 +6,39 @@ import AcademicFilter from "../../components/filter_components/academics/academi
 import InternshipFilter from "../../components/filter_components/internship/internship.component";
 
 const Filters = () => {
-    return (
-        <div className="filters__page">
-            <h1>FILTERS PAGE</h1>
-             
-             <FilterFaculty />
-             <CurricularFilter />
-             <AcademicFilter />
-             <InternshipFilter />
+    const [currCom, setCurrCom] = useState();
+  const mapComponents = [
+    {component: <FilterFaculty />, title: "Faculty", id: 1},
+    {component: <CurricularFilter />, title: "Curricular", id: 2},
+    {component: <AcademicFilter />, title: "Academic", id: 3},
+    {component: <InternshipFilter />, title: "Internship", id: 4},
+  ];
+
+  const handleClick = (component) => {
+    setCurrCom(component);
+  }
+  return (
+    <div className="filters__page">
+      <h1>FILTERS PAGE</h1>
+      <div className="form__wrapper">
+        <div className="display__filter__form">
+            {
+                currCom ? currCom : <p>PLEASE CHOOSE A CATEGORY FROM BELOW</p>
+            }
         </div>
-    )   
-}
+      </div>
+
+      <div className="filter__scroller">
+        {
+            mapComponents.map(item => (
+                <div className="particular__filter__title" onClick={() => {handleClick(item.component)}}>
+                    <p>{item.title}</p>
+                </div>
+            ))
+        }
+      </div>
+    </div>
+  );
+};
 
 export default Filters;

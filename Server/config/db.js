564f6683f52a71d29f941fd0e76123ee68039f18
Dhruv Dave <78278281+DhruvDave12@users.noreply.github.com>
2022-04-29 14:06:28 +0530

    added user data in process.env

--- Server/config/db.js
+++ Server/config/db.js
@@ -3,9 +3,9 @@ const mysql = require('mysql2');
 const connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
-    database: "studentinfosys",
-    user: "root",
-    password: "Akshat123@#",
+    database: process.env.DATABASE_NAME,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
     multipleStatements: true
 })
 
